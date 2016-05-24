ecfe5fa35f1dc380902758b76c5023cb0398480d
Emily Chen <emily.c.chen@gmail.com>
2016-05-24 16:52:09 -0400

    Refactor separate getResults function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,30 @@
 $(document).ready(function() {
-	$.getJSON("https://www.googleapis.com/youtube/v3/search/?part=snippet&key=AIzaSyAxtqf8F_Mrn0R5-_xVtuJqYBEmblG_Y2I&q=yoga", function(data){
-		var myData = data.items;
-		$.each(myData, function(index, value) {
-			console.log(value.snippet.thumbnails.default.url);
-		});
-	});
+	// var searchTerm = "cupcakes";
+	// $("#search-form").submit(function(event) {
+	// 	event.preventDefault();
+	// 	searchTerm = $("#search-word").val();
+	// 	getRequest(searchTerm);
+	// });
 });
+
+
+function showResults(results) {
+	var html = "";
+	$.each(results, function(index, value) {
+		html += "<img src='" + value.snippet.thumbnails.default.url + "'>";
+		console.log(value.snippet.thumbnails.default.url);
+	});
+	$("#search-results").html(html);
+};
+
+function getRequest(searchTerm) {
+	var params = {
+		part: "snippet",
+		key: "AIzaSyAxtqf8F_Mrn0R5-_xVtuJqYBEmblG_Y2I",
+		q: searchTerm
+	};
+	url = "https://www.googleapis.com/youtube/v3/search/?";
+	$.getJSON(url, params, function(data) {
+		showResults(data.items);
+	});
+};
