f52fba95666dcebb60d41b8c7e62adcec354c43c
Emily Chen <emily.c.chen@gmail.com>
2016-05-24 16:12:20 -0400

    Add HTML to show image thumbnails

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
-	$.getJSON("https://www.googleapis.com/youtube/v3/search/?part=snippet&key=AIzaSyAxtqf8F_Mrn0R5-_xVtuJqYBEmblG_Y2I&q=yoga", function(data){
-		var myData = data.items;
-		$.each(myData, function(index, value) {
-			console.log(value.snippet.thumbnails.default.url);
-		});
+	$.getJSON("https://www.googleapis.com/youtube/v3/search/?part=snippet&key=AIzaSyAxtqf8F_Mrn0R5-_xVtuJqYBEmblG_Y2I&q=yoga", function(data) {
+		showResults(data.items);
 	});
 });
+
+function showResults(results) {
+	var html = "";
+	$.each(results, function(index, value) {
+		html += "<img src='" + value.snippet.thumbnails.default.url + "'>";
+		console.log(value.snippet.thumbnails.default.url);
+	});
+	$("#search-results").html(html);
+};
