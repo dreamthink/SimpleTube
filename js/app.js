9c66845a548816a5247d8afe9b587e6032498ec3
Emily Chen <emily.c.chen@gmail.com>
2016-05-24 20:50:04 -0400

    Add working search function

--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
-$(document).ready(function() {
-	// var searchTerm = "cupcakes";
-	// $("#search-form").submit(function(event) {
-	// 	event.preventDefault();
-	// 	searchTerm = $("#search-word").val();
-	// 	getRequest(searchTerm);
-	// });
-});
+var searchTerm = "";
 
+$(document).ready(function() {
+	$("#search-form").submit(function(event) {
+		event.preventDefault();
+		searchTerm = $("#search-word").val();
+		getRequest(searchTerm);
+	});
+});
 
 function showResults(results) {
 	var html = "";
 	$.each(results, function(index, value) {
-		html += "<img src='" + value.snippet.thumbnails.default.url + "'>";
-		console.log(value.snippet.thumbnails.default.url);
+		html += "<li><img src='" + value.snippet.thumbnails.medium.url + "'></li>";
+		console.log(value.snippet.thumbnails.medium.url);
 	});
-	$("#search-results").html(html);
+	$("#search-results ul").html(html);
 };
 
 function getRequest(searchTerm) {
+	url = "https://www.googleapis.com/youtube/v3/search/?";
 	var params = {
 		part: "snippet",
 		key: "AIzaSyAxtqf8F_Mrn0R5-_xVtuJqYBEmblG_Y2I",
 		q: searchTerm
 	};
-	url = "https://www.googleapis.com/youtube/v3/search/?";
 	$.getJSON(url, params, function(data) {
 		showResults(data.items);
 	});
