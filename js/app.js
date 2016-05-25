1ba1d874202b1a6ccd1e6d62b5584d5483d39fba
Emily Chen <emily.c.chen@gmail.com>
2016-05-24 21:42:07 -0400

    Clear search box after form submission, add links to video

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
-var searchTerm = "";
-
 $(document).ready(function() {
 	$("#search-form").submit(function(event) {
 		event.preventDefault();
-		searchTerm = $("#search-word").val();
+		var searchTerm = $("#search-word").val();
 		getRequest(searchTerm);
+		$("#search-form")[0].reset();
 	});
 });
 
 function showResults(results) {
 	var html = "";
 	$.each(results, function(index, value) {
-		html += "<li><img src='" + value.snippet.thumbnails.medium.url + "'></li>";
+		html += "<li><a href='https://www.youtube.com/watch?v=" + encodeURIComponent(value.id.videoId) + "'>" + "<img src='" + value.snippet.thumbnails.medium.url + "'></a></li>";
 		console.log(value.snippet.thumbnails.medium.url);
 	});
 	$("#search-results ul").html(html);
@@ -28,3 +27,13 @@ function getRequest(searchTerm) {
 		showResults(data.items);
 	});
 };
+
+
+
+
+// $(document).ready(function() {
+// 	$.getJSON("https://www.googleapis.com/youtube/v3/search/?part=snippet&key=AIzaSyAxtqf8F_Mrn0R5-_xVtuJqYBEmblG_Y2I&q=meditation", function(data) {
+// 		console.log(data);
+// 	});
+// });
+
